55e323680aa6383d355ee39950aa54a04a6358d7
billy <zijian@student.umass.edu>
2013-04-20 15:01:22 -0400

    update

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 /*
  * GET home page.
@@ -7,6 +7,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
-=======
-,/*, * GET home page., */,,exports.index = function(req, res){,  res.render('index', { title: 'Express' });,};
->>>>>>> 196500d9095b1d7d1bd5923f551700ee1596d753
+
+/*, * GET home page., */
+xports.index = function(req, res){,  res.render('index', { title: 'Express' });,};
+
